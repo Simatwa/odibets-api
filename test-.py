d58ea524871bd14144d2dbd4de1b04e36bd95e04
smartwa <smartwacaleb@gmail.com>
2024-06-03 15:07:16 +0300

    feat: Update.gitignore and requirements.txt; Modify test.py

--- test-.py
+++ test-.py
@@ -2,7 +2,7 @@ from odibets_api import Odibets
 
 inf = Odibets().daily_jackpot()
 
-print(inf.jackpots[0])
+print(inf.matches)
 
 for match in inf.matches:
     print(match.sport_id, match.home_team, match.away_team, match.start_time, sep=" - ")
